9db25166bc8d7beff5c0173ff043bc8877bbca06
ianoli12 <ian.oliveira@hotmail.com>
2021-03-03 23:23:25 -0300

    alteracao2

--- principal_adworks_mysql.py
+++ principal_adworks_mysql.py
@@ -8,13 +8,10 @@ import pandas as pd
 import sqlalchemy as sa
 import MySQLdb
 import prettytable
-pt = PrettyTable()
 
-"""engine = sa.create_engine('mssql://DESKTOP-EK0ORPP/AdventureWorks2017?trusted_connection=yes')
-query = 'SELECT top 4 PRODUCTID,NAME from Production.Product'
-df = pd.read_sql(query,engine)
-print(df.head(4))"""
 
 conn_mysqldb = MySQLdb.connect(host="127.0.0.1",port=3306,user="ianoli12",passwd="32244000",db="AdventureWorks")
 df = pd.read_sql('SELECT PRODUCTID,NAME,PRODUCTNUMBER,COLOR,LISTPRICE,WEIGHT from Product',conn_mysqldb)
-print(df.head(4))
+print("#####################################################################################")
+print(df.head(4).to_string(index=False))
+print("#####################################################################################")
