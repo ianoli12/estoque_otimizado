76ac1b08f6434900efab0f565685ec84b0fc0683
Ian Oliveira <ian.oliveira@elgin.com.br>
2022-10-27 09:35:27 -0300

    testando1

--- principal.py
+++ principal.py
@@ -24,63 +24,37 @@ def modulo_est(conn):
         print("5 - Sair\n")
         op_moduloest = int(input("Digite a opção desejada> "))
 
-        #FAZ SELECT SIMPLES 
-        #def select_tab(conn):
-         #    cursor = conn.cursor()
-          #   cursor.execute('select * from PRODUTOS')
-
+        # CONSULTANDO PRODUTOS
         def modest_consprod(conn):
             pergunta = "s"
             
             while(pergunta == "s" and "S"):
                 print("\nConsulta de Produtos:\n")
-                #MOSTRA UM SIMPLES SELECT DA TABELA
                 cursor = conn.cursor()
-                #cursor.execute('SELECT TOP 10 PRODUCTID,NAME,PRODUCTNUMBER,COLOR,SAFETYSTOCKLEVEL,STANDARDCOST,LISTPRICE from Production.Product')
-                #lista = list(cursor.execute('SELECT top 10 PRODUCTID,NAME from Production.Product'))
                 df = pd.read_sql('SELECT TOP 10 PRODUCTID,NAME,PRODUCTNUMBER,Makeflag from Production.Product order by PRODUCTID',conn)
                 print(colored(df.to_string(index=False),"blue"))
 
-                #MOSTRA UM  SELECT APENAS DO CÓDIGO MENCIONADO
                 CODPROD = int(input("Digite o código do produto: \n"))
                 cursor.execute('select PRODUCTID from PRODUCTION.PRODUCT where PRODUCTID=?',CODPROD)
                 
                 prod_existe = 0
-                
-                #for x in df:
-                #print(df.iloc[df[0],[0]].to_string(index=False))
-                #if df['PRODUCTID'].values(CODPROD) == CODPROD:
-                #print(np.where(df.index==CODPROD)[0])
-                #df = productid.values[CODPROD]
-                #print(df.values(1,[CODPROD])
-                #if np.where(df.values([[1],[CODPROD]]):
                 res = 0
                 for x in cursor:
                     res = int(''.join(map(str,x)))
-                    #print(res)
-               # c = df['PRODUCTID']
-                #c[[0]]
-                #print(int(c.iloc[CODPROD-1]))
-                #d = df.convert_dtypes(d)
-                #df = pd.DataFrame()
-                #if c.iloc[CODPROD-1] == CODPROD:
+
                 if res == CODPROD:
                     print("Dados do Produto:\n")
-                    #cursor.execute('select PRODUCTID,NAME,PRODUCTNUMBER,Makeflag from Production.Product where PRODUCTID=?',CODPROD)
-                    #d = cursor.fetchone()
-                    #df.convert_dtypes(d)
                     print(df.loc[df['PRODUCTID']==res,['PRODUCTID','NAME','PRODUCTNUMBER','Makeflag']].to_string(index=False))
                 else:
                     print("Produto não encontrado")
-                    #print(df.iloc[CODPROD])
                 pergunta = str(input("\nDeseja consultar novamente?(s/n)...\n"))
 
+        # ADICIONANDO PRODUTOS
         def modest_addprod(conn):
             pergunta = "s"
             cursor = conn.cursor()
             while(pergunta == "s" and "S"):
                 prod_igual = ""
-                #PRODUCTID = 0
                 df2 = 0
                 res = 0
                 
@@ -88,7 +62,6 @@ def modulo_est(conn):
                     res = int(''.join(map(str,x)))
 
                 while(prod_igual == ""):
-                    #print(df2.loc[df2['ProductID']==PRODUCTID,['ProductID']].to_int(index=False))
                     PRODUCTID = int(input("\nDigite o código do produto a ser incluído: \n"))
                     if PRODUCTID == df2.loc[df2['ProductID']==PRODUCTID,['ProductID']].to_string(index=False):
                         print(colored("O Código deste produto já está cadastrado","red"))
@@ -110,12 +83,11 @@ def modulo_est(conn):
                 cursor.execute('SET IDENTITY_INSERT [Production].[Product] ON;INSERT INTO Production.Product(ProductID,Name,ProductNumber,MakeFlag,FinishedGoodsFlag,Color,SafetyStockLevel,ReorderPoint,StandardCost,ListPrice,DaysToManufacture,SellStartDate) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)',PRODUCTID,NAME,PRODUCTNUMBER,MAKEFLAG,FINISHEDGOODSFLAG,COLOR,SAFETYSTOCKLEVEL,REORDERPOINT,STANDARDCOST,LISTPRICE,DAYSTOMANUFACTURE,SELLSTARTDATE)
                 conn.commit()
                 df2 = pd.read_sql('SELECT ProductID,Name,ProductNumber,MakeFlag,FinishedGoodsFlag,Color,SafetyStockLevel,ReorderPoint,StandardCost,ListPrice,DaysToManufacture,SellStartDate FROM PRODUCTION.PRODUCT ORDER BY PRODUCTID',conn)
-                #Abaixo é necessário colocar o nome das colunas EXATAMENTE como está no banco de dados(usando mauísculo ou minusculo)
                 print(colored(df2.loc[df2['ProductID']==PRODUCTID,['ProductID','Name','ProductNumber','MakeFlag','FinishedGoodsFlag','Color','SafetyStockLevel','ReorderPoint','StandardCost','ListPrice','DaysToManufacture','SellStartDate']].to_string(index=False),"blue"))
 
                 pergunta = str(input("\nDeseja INCLUIR o produto novamente?...\n"))
 
-
+        # ALTERANDO PRODUTOS
         def modest_altprod(conn): 
             print("\Alteração de Produtos:\n")
             pergunta = "s"
@@ -136,14 +108,6 @@ def modulo_est(conn):
                     print(df.loc[df['ProductID']==res,['ProductID','Name','ProductNumber','MakeFlag']].to_string(index=False))
                 else:
                     print("Produto não encontrado")
-
-                #cursor = conn.cursor()
-                #cursor.execute('SELECT ProductID,Name,ProductNumber,MakeFlag,FinishedGoodsFlag,Color,SafetyStockLevel,ReorderPoint,StandardCost,ListPrice,DaysToManufacture,SellStartDate FROM Production.Product Where ProductID=?',PRODUCTID)
-                #df1 = DataFrame()
-
-                
-                
-                #print(df.loc[df['ProductID']==PRODUCTID,['ProductID','Name','ProductNumber','MakeFlag','FinishedGoodsFlag','Color','SafetyStockLevel','ReorderPoint','StandardCost','ListPrice','DaysToManufacture','SellStartDate']].to_string(index=False))
                 print("Preencha os valores a serem alterados\n")
 
                 NAME = str(input("\n Novo nome: \n"))
@@ -158,16 +122,12 @@ def modulo_est(conn):
                 DAYSTOMANUFACTURE = int(input("\nNova quantidade de dias para a manufatura do produto: \n"))
                 SELLSTARTDATE = str(input("\n Nova Data Inicial de venda do produto: (Ex:2008-04-30): \n"))
 
-                cursor = conn.cursor()
-                #str(PRODUCTID)
-                
+                cursor = conn.cursor()       
                 cursor.execute('SET IDENTITY_INSERT [Production].[Product] ON;UPDATE Production.Product SET Name=?,ProductNumber=?,MakeFlag=?,FinishedGoodsFlag=?,Color=?,SafetyStockLevel=?,ReorderPoint=?,StandardCost=?,ListPrice=?,DaysToManufacture=?,SellStartDate=? WHERE ProductID = ?',NAME,PRODUCTNUMBER,MAKEFLAG,FINISHEDGOODSFLAG,COLOR,SAFETYSTOCKLEVEL,REORDERPOINT,STANDARDCOST,LISTPRICE,DAYSTOMANUFACTURE,SELLSTARTDATE,PRODUCTID)
-                #cursor.execute('UPDATE Production.Product SET Name=? WHERE ProductID = ?',NAME,PRODUCTID)                
                 conn.commit()
-                #int(PRODUCTID)
-
                 pergunta = str(input("\nDeseja ALTERAR o produto novamente?...\n"))     
 
+        # DELETANDO PRODUTOS
         def modest_delprod(conn):
 
             print("\Remover Produtos:\n")
@@ -195,9 +155,6 @@ def modulo_est(conn):
                     print("Produto não encontrado")
                 pergunta = str(input("\nDeseja EXCLUIR outro produto?...\n"))
 
-
-
-
         if op_moduloest == 1:
             modest_consprod(conn)
         elif op_moduloest == 2:
@@ -209,7 +166,7 @@ def modulo_est(conn):
         else:
             print("A opção precisa ser numérica")
 
-
+# SOBRE O SOFTWARE
 def sobre_software():
     print("Este software foi produzido por Ian Oliveira")
 
@@ -220,7 +177,7 @@ conn = pyodbc.connect(
     "Trusted_Connection=yes;"
 )
 
-#print(bcolors.WARNING+"BEM VINDO AO ESTOQUE BRASIL\n\n")
+# MENU PRINCIPAL
 print(colored("\n\n\n\n\n\n---- [ BEM VINDO AO ESTOQUE OTIMIZADO ] ----\n\n",'green'))
 
 print("1 - Módulo de Estoque: \n")
